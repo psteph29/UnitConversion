8eea6211100c0f1f01d4cdde2874978dda1f2fe9
Paige Stephenson <paigestephenson29@gmail.com>
2023-06-07 11:00:30 -0600

    Initial Commit

---
+++ ConversionChallengeDay19/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConversionChallengeDay19
+//
+//  Created by Paige Stephenson on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConversionChallengeDay19/ConversionChallengeDay19App.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionChallengeDay19App.swift
+//  ConversionChallengeDay19
+//
+//  Created by Paige Stephenson on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionChallengeDay19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
